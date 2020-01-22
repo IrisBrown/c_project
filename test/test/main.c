e2a70c4533a918004f33db773025246a4fffac9d
IrisBrown <2719407063@qq.com>
2020-01-22 21:34:13 +0800

    first

---
+++ test/test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by pro on 2020/1/22.
+//  Copyright © 2020 pro. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
