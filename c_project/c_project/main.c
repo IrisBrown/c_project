4395fbfa1c4c575db5974ee2ffa30672ffbf2d55
IrisBrown <2719407063@qq.com>
2020-01-26 20:09:50 +0800

    用指针计算（修改版）

--- c_project/c_project/main.c
+++ c_project/c_project/main.c
@@ -12,7 +12,7 @@
 int main(void)
 {
     //定义一个rain数组存放降雨量数据，行为年，列为月
-    const float rain[YEARS][MONTHS]=
+    float rain[YEARS][MONTHS]=
     {
         {4.3,4.3,4.3,3.0,2.0,1.2,0.2,0.2,0.4,2.4,3.5,6.6},
         {8.5,8.2,1.2,1.6,2.4,0.0,5.2,0.9,0.3,0.9,1.4,7.3},
@@ -22,16 +22,16 @@ int main(void)
     };
     int year,month;
     float subtot,total;
+    float (*p)[MONTHS]=rain;
     //输出表头
     printf(" YEAR       RAINFALL (inches)\n");
     //计算每年的各月降水量之和，输出年份和年降水量，并计算总降水量
     for(year=0,total=0;year<YEARS;year++)
-     {
+    {
         for(month=0,subtot=0;month<MONTHS;month++)
-            subtot+=rain[year][month];
-        printf("%5d %15.lf\n",2010+year,subtot);
-        total+=subtot;
-     }
+            subtot+=p[year][month];
+        printf("%5d  %15.lf\n",2010+year,subtot);
+    }
     //输出年平均降水量
     printf("\nThe yearly aveage is %.lf inches.\n\n",total/YEARS);
     printf("MONTHLY AVERAGES:\n\n");
@@ -41,9 +41,10 @@ int main(void)
     for(month=0;month<MONTHS;month++)
     {
         for(year=0,subtot=0;year<YEARS;year++)
-            subtot+=rain[year][month];
+            subtot+=p[year][month];
         printf("%4.lf",subtot/YEARS);
     }
     printf("\n");
     return 0;
 }
+
